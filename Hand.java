7656f58da044c230f521e2e1f731e66b7af71645
Owner <ashwithaala@gmail.com>
2022-10-21 15:29:42 -0400

    Hi

--- Hand.java
+++ Hand.java
@@ -1,2 +1,15 @@
-public class Hand {
-}
+import java.util.*;
+/**
+ *
+ */
+public class Hand
+{
+    private final List<Letter> letters;
+    public Hand(String l)
+    {
+        letters = new ArrayList<Letter>();
+        String[] split = l.split(" ");
+        for (String s: split) {
+            letters.add(new Letter(s));
+        }
+    }
--- Player.java
+++ Player.java
@@ -1,2 +1,32 @@
-public class Player {
+import java.util.*;
+/**
+ * Player class
+ */
+public class Player{
+    int score;
+    private String name;
+    private ArrayList<LetterTile> letters;
+    public Player()
+    {
+        this.letters = new ArrayList<>(7);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String setName(String playerName){
+        this.name = playerName;
+        return playerName;
+    }
+
+    public int GetScore(){
+        return this.score;
+    }
+
+    public int SetScore(int scoreVal){
+        this.score = scoreVal;
+        return scoreVal;
+    }
+
 }
