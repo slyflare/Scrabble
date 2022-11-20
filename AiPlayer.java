b4afcf108736038b3d2f1dccb7448a2df4eea4a1
Vimal Gunasegaran <55605963+slyflare@users.noreply.github.com>
2022-11-20 14:20:17 -0500

    Smol stuff

--- AiPlayer.java
+++ AiPlayer.java
@@ -1,10 +1,26 @@
+import java.util.ArrayList;
+
 public class AiPlayer extends Player{
 
-    public AiPlayer(String name) {
+    private ArrayList<String> wordBank;
+
+    public AiPlayer(String name, ArrayList<String> wordBank) {
         super(name);
+        this.wordBank = wordBank;
     }
 
-    private void makeMove(){
+    private void makeMove(Board board){
 
     }
+
+    private ArrayList<String> checkHorizontalPossibilities(){
+
+        return null;
+    }
+
+    private ArrayList<String> checkVerticalPossibilities(){
+
+        return null;
+    }
+
 }
