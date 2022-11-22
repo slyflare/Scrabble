edbc5fe365b6e5778e50bec4ea27556e58ea9504
Owner <ashwithaala@gmail.com>
2022-11-22 16:23:05 -0500

    reverted back

--- Scrabble.java
+++ Scrabble.java
@@ -76,7 +76,7 @@ public class Scrabble {
     public void createLetterBag() {
         int i;
         char[] letters = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
-                'O','P','Q','R','S','T','U','V','W','X','Y','Z', '_'};
+                'O','P','Q','R','S','T','U','V','W','X','Y','Z', ' '};
         int[] numTiles = {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1,2};
         for(i = 0; i < 27; i++) {
             for(int j = 0; j < numTiles[i]; j++) {
