90f6f96d37e6c0434bf1deeb0843aedbace682e7
Vimal Gunasegaran <55605963+slyflare@users.noreply.github.com>
2022-11-21 20:18:15 -0500

    Moved premium to Board

--- Board.java
+++ Board.java
@@ -11,10 +11,51 @@ public class Board {
    private int col;
    private char input;
 
+    private HashMap<ArrayList<Integer>,Integer> premium;
+
     public Board(){
         this.board();
-        //this.updateBoard();
 
+        //3LS
+        premium.put(new ArrayList<>(Arrays.asList(1,5)),3);
+        premium.put(new ArrayList<>(Arrays.asList(5,5)),3);
+        premium.put(new ArrayList<>(Arrays.asList(9,5)),3);
+        premium.put(new ArrayList<>(Arrays.asList(13,5)),3);
+        premium.put(new ArrayList<>(Arrays.asList(5,1)),3);
+        premium.put(new ArrayList<>(Arrays.asList(9,1)),3);
+        premium.put(new ArrayList<>(Arrays.asList(1,9)),3);
+        premium.put(new ArrayList<>(Arrays.asList(5,9)),3);
+        premium.put(new ArrayList<>(Arrays.asList(9,9)),3);
+        premium.put(new ArrayList<>(Arrays.asList(13,9)),3);
+        premium.put(new ArrayList<>(Arrays.asList(5,13)),3);
+        premium.put(new ArrayList<>(Arrays.asList(9,13)),3);
+
+
+        //2LS
+        premium.put(new ArrayList<>(Arrays.asList(0,3)),2);
+        premium.put(new ArrayList<>(Arrays.asList(0,11)),2);
+        premium.put(new ArrayList<>(Arrays.asList(2,5)),2);
+        premium.put(new ArrayList<>(Arrays.asList(2,8)),2);
+        premium.put(new ArrayList<>(Arrays.asList(3,0)),2);
+        premium.put(new ArrayList<>(Arrays.asList(3,7)),2);
+        premium.put(new ArrayList<>(Arrays.asList(3,14)),2);
+        premium.put(new ArrayList<>(Arrays.asList(6,2)),2);
+        premium.put(new ArrayList<>(Arrays.asList(6,6)),2);
+        premium.put(new ArrayList<>(Arrays.asList(6,8)),2);
+        premium.put(new ArrayList<>(Arrays.asList(6,12)),2);
+        premium.put(new ArrayList<>(Arrays.asList(7,3)),2);
+        premium.put(new ArrayList<>(Arrays.asList(7,11)),2);
+        premium.put(new ArrayList<>(Arrays.asList(8,2)),2);
+        premium.put(new ArrayList<>(Arrays.asList(8,6)),2);
+        premium.put(new ArrayList<>(Arrays.asList(8,8)),2);
+        premium.put(new ArrayList<>(Arrays.asList(8,12)),2);
+        premium.put(new ArrayList<>(Arrays.asList(11,0)),2);
+        premium.put(new ArrayList<>(Arrays.asList(11,7)),2);
+        premium.put(new ArrayList<>(Arrays.asList(11,14)),2);
+        premium.put(new ArrayList<>(Arrays.asList(12,6)),2);
+        premium.put(new ArrayList<>(Arrays.asList(12,8)),2);
+        premium.put(new ArrayList<>(Arrays.asList(14,3)),2);
+        premium.put(new ArrayList<>(Arrays.asList(14,11)),2);
 
     }
     /**Initialising the scrabble board with 15 numbered rows and 15 alphabetized columns.
@@ -27,6 +68,10 @@ public class Board {
                 this.board[i][j] = ' ';
             }
         }
+    }
+
+    public HashMap<ArrayList<Integer>, Integer> getPremium() {
+        return premium;
     }
 
     /**
